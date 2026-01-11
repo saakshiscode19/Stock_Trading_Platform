53d7cb81b618b971df74eda72070d403ccefa091
Saakshi Sharma <193083750+saakshiscode19@users.noreply.github.com>
2026-01-11 17:05:35 +0530

    new update

---
+++ frontend/src/landing_page/home/HomePage.js
@@ -0,0 +1,30 @@
+
+
+import React from 'react';  
+
+import Awards from './Awards';
+import Hero from './Hero';
+import Stats from './Stats';
+import Pricing from './Pricing';
+import Education from './Education';
+import OpenAccount from '../OpenAccount';
+import Navbar from '../Navbar';
+
+import Footer from '../Footer';
+
+function HomePage() {
+   return(  <>
+      <Navbar />
+      <Hero />
+      <Awards />
+      <Stats />
+      <Education />
+      <Footer />
+      <Pricing />
+      <OpenAccount />
+
+    </>
+   );
+}
+
+export default HomePage;
---
+++ frontend/src/landing_page/support/RaiseTicket.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function RaiseTicket() {
+    return ( 
+        <h1>RaiseTicket</h1>
+     );
+}
+
+export default RaiseTicket;
